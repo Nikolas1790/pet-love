73cfdb8ac0377760e9edfac7add9947ce3ca149b
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-04-19 23:00:05 +0300

    add-redax

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,11 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import Loader from 'components/Loader/Loader';
 import Header from 'components/Header/Header';
 
 export default function Layout() {
-  const location = useLocation();
-  const hideHeaderPaths = ['/register', '/login'];
+  // const location = useLocation();
+  // const hideHeaderPaths = ['/register', '/login'];
   return (
     <div>
       {/* {hideHeaderPaths.includes(location.pathname) ? null : <Header />} */}
