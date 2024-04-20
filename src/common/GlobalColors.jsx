d9dd7ad1e07b39268ba1bb38de32c2b465295778
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-04-20 18:49:12 +0300

    correct-bcg-color

--- src/common/GlobalColors.jsx
+++ src/common/GlobalColors.jsx
@@ -6,7 +6,8 @@ const color = {
   yellowLight: 'rgb(255, 244, 223)',
 
   wight: 'rgb(255, 255, 255)',
-  mainBackground: ' rgb(204, 204, 204)',
+  // mainBackground: ' rgb(204, 204, 204)',
+  mainBackground: ' rgb(249, 249, 249)',
 
   // blackModalColr: 'rgba(29, 30, 33, 0.8)',
 };
