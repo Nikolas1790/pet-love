03dfd2603663cdf3f4d65d1f10671547995be842
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-04-28 10:44:43 +0000

    Deploying to gh-pages from @ Nikolas1790/pet-love@04a05d496791afaf75e952b4d6fa5e56eb23ad78 🚀

--- static/js/536.b04f80df.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[536],{2536:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var r=n(5245),u=n(184);function c(){return(0,u.jsx)(r.OO,{children:"Newsss"})}function s(){return(0,u.jsx)(c,{})}}}]);
-//# sourceMappingURL=536.b04f80df.chunk.js.map
