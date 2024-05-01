72a8e1a357f5b004cb403508d0bbd90a5dba2a6a
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-05-01 18:02:17 +0300

    pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,111 +1,44 @@
-// import { PaginationButton, PaginationConteiner, PaginationSvg } from "./Pagination.styled";
-// import sprite from '../../img/sprite.svg';
-
-// export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-//   const pages = [...Array(totalPages).keys()].map(num => num + 1);
-
-//   const goToPreviousPage = () => {
-//     if (currentPage > 1) {
-//       onPageChange(currentPage - 1);
-//     }
-//   };
-
-//   const goToNextPage = () => {
-//     if (currentPage < totalPages) {
-//       onPageChange(currentPage + 1);
-//     }
-//   };
-
-//   return (
-//     <PaginationConteiner>
-//       <PaginationButton
-//         disabled={currentPage === 1}
-//         current='true'
-//         onClick={goToPreviousPage}
-//       >
-//         <PaginationSvg>
-//           <use href={`${sprite}#icon-slider-l-left`} />
-//         </PaginationSvg>
-//       </PaginationButton>
-
-//       {pages.map(page => (
-//         <PaginationButton
-//           key={page}
-//           disabled={page === currentPage}
-//           onClick={() => onPageChange(page)}
-//         >
-//           {page}
-//         </PaginationButton>
-//       ))}      
-
-//       <PaginationButton
-//         disabled={currentPage === totalPages}
-//         current='last'
-//         onClick={goToNextPage}
-//       >
-//         <PaginationSvg >
-//           <use href={`${sprite}#icon-slider-r-right`} />
-//         </PaginationSvg>
-//       </PaginationButton>
-//     </PaginationConteiner>
-//   );
-// };
-
-
-
 import { PaginationButton, PaginationConteiner, PaginationSvg } from "./Pagination.styled";
 import sprite from '../../img/sprite.svg';
 
 export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  // const pages = [...Array(totalPages).keys()].map(num => num + 1);
+  const maxPageNumberLimit = 5;
 
-  // const goToPreviousPage = () => {
-  //   if (currentPage > 1) {
-  //     onPageChange(currentPage - 1);
-  //   }
-  // };
+  const pages = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
 
-  // const goToNextPage = () => {
-  //   if (currentPage < totalPages) {
-  //     onPageChange(currentPage + 1);
-  //   }
-  // };
-// Ограничение количества отображаемых страницных кнопок
-const maxPageNumberLimit = 5;
-const minPageNumberLimit = 0;
+  const handlePrevBtn = () => {
+    onPageChange(currentPage - 1);
+  };
 
-const pages = [];
-for (let i = 1; i <= totalPages; i++) {
-  pages.push(i);
-}
+  const handleNextBtn = () => {
+    onPageChange(currentPage + 1);
+  };
 
-const handlePrevBtn = () => {
-  onPageChange(currentPage - 1);
-};
+  const handleFirstPage = () => {
+    onPageChange(1);
+  };
 
-const handleNextBtn = () => {
-  onPageChange(currentPage + 1);
-};
-
-const handleFirstPage = () => {
-  onPageChange(1);
-};
-
-const handleLastPage = () => {
-  onPageChange(totalPages);
-};
-
+  const handleLastPage = () => {
+    onPageChange(totalPages);
+  };
+console.log(pages)
   return (
     <PaginationConteiner>
       <PaginationButton disabled={currentPage === 1} onClick={handleFirstPage}>
-        ««
+      <PaginationSvg>
+           <use href={`${sprite}#icon-slider-l-left`} />
+        </PaginationSvg>
       </PaginationButton>
       <PaginationButton disabled={currentPage === 1} onClick={handlePrevBtn}>
-        «
+         <PaginationSvg>
+           <use href={`${sprite}#icon-slider-left`} />
+        </PaginationSvg>
       </PaginationButton>
-      {currentPage > 1 && totalPages > maxPageNumberLimit && <span>...</span>}
       {pages.map(page => {
-        if (page < currentPage + 2 && page > currentPage - 2) {
+        if (page < currentPage + 3 && page > currentPage - 2) {
           return (
             <PaginationButton
               key={page}
@@ -121,10 +54,14 @@ const handleLastPage = () => {
       })}
       {currentPage < totalPages && totalPages > maxPageNumberLimit && <span>...</span>}
       <PaginationButton disabled={currentPage === totalPages} onClick={handleNextBtn}>
-        »
+         <PaginationSvg >
+           <use href={`${sprite}#icon-slider-right`} />
+         </PaginationSvg>
       </PaginationButton>
       <PaginationButton disabled={currentPage === totalPages} onClick={handleLastPage}>
-        »»
+         <PaginationSvg >
+/           <use href={`${sprite}#icon-slider-r-right`} />
+         </PaginationSvg>
       </PaginationButton>
     </PaginationConteiner>
   );
