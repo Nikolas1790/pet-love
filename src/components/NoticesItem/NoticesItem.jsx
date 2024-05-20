bc2e8d8edec5cb4ad2fcba8f1163a548737c87cc
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-05-20 23:05:18 +0300

    logic-add-to-favorit-on-notice-item

--- src/components/NoticesItem/NoticesItem.jsx
+++ src/components/NoticesItem/NoticesItem.jsx
@@ -7,16 +7,16 @@ import { useState } from "react";
 import ModalAttentions from "components/Modals/ModalAttention/ModalAttention";
 import ModalNotice from "components/Modals/ModalNotice/ModalNotice";
 import { selectToken } from "../../redux/auth/selectorAuth";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Star } from "components/StarRaiting/StarRaiting.styled";
+import { noticesFavorite } from "../../redux/petLove/operations";
 
 export default function NoticesItem({card}) {  
   const [openModal, setOpenModal] = useState(false);
   const [openModalNotice, setOpenModalNotice] = useState(false);
-
   const token = useSelector(selectToken);
-
-  // console.log(card)
+  const dispatch = useDispatch();
+  console.log(card)
 
   // Функція для форматування дати
   const formatDate = (dateString) => {
@@ -38,7 +38,10 @@ export default function NoticesItem({card}) {
 
   const handleButtonClick = () => {
     if (token) {
-      setOpenModalNotice(true); // Если токен есть, открываем первую модалку
+      // setOpenModalNotice(true);
+
+      dispatch(noticesFavorite(card._id));
+
     } else {
       setOpenModal(true); // Если токена нет, открываем вторую модалку
     }
