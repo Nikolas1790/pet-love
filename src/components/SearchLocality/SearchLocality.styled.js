2e107a1b8ae8daa0831290e5a40295b527730512
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-06-06 19:18:25 +0300

    react-select

--- src/components/SearchLocality/SearchLocality.styled.js
+++ src/components/SearchLocality/SearchLocality.styled.js
@@ -1,47 +1,88 @@
-import color from 'common/GlobalColors';
+import Select from 'react-select';
 import styled from 'styled-components';
 
-export const SearchContainer = styled.div`
-position: relative;
-`;
-
-export const SearchInput = styled.input`
-  padding: 14px;
-  /* display: flex;
-    align-self: center;
-  justify-content: center; */
-  font-size: 16px;
-  width: ${({width}) => width || '230px' };
-  border: ${({ border }) => border || `1px solid ${color.blackPrimaryLight}`};
+export const StyledSelect = styled(Select)`
+.react-select__control {
+  width: 227px;
+  height: 48px;
+  border: none;
   border-radius: 30px;
-  outline: none;
+  background: rgb(255, 255, 255);
+  box-shadow: none;
+  padding-left: 14px;
+}
 
-  &::placeholder {
-    font-size: 16px;
-    font-weight: 500;
-    color: ${({ border }) => border === "none" ? color.blackPrimary :  "none"};
-  }
+.react-select__control--is-focused {
+  border-color: 1px solid rgb(246, 184, 61);
+}
 
+.react-select__value-container {
+  margin: 0;
+  padding: 0;
+}
+
+.react-select__value-container {
+  padding: 0 6px;
+  margin: 0;
+  padding: 0;
+}
+
+.react-select__input {
+  /* margin: 0;
+  padding: 0; */
+}
+
+.react-select__placeholder {
+  color: rgba(0, 0, 0, 0.4);
+  margin: 0;
+  padding: 0;
+}
+
+.react-select__indicator-separator {
+  display: none;
+}
+
+.react-select__dropdown-indicator {
+  /* padding: 4px; */
+  display: none;
+}
+
+.react-select__menu {
+  border-radius: 15px;
+  margin-top: -84px;
+}
+
+.react-select__menu-list {
+  border-radius: 15px;
+}
 `;
 
-export const SearchButton = styled.button`
-  position: absolute;
-  top: 15px;
-  right: 15px;
+
+
+export const Button = styled.button`
+  margin: 10px;
+  padding: 10px 20px;
+  background-color: rgb(246, 184, 61);
+  border: none;
+  border-radius: 5px;
+  color: white;
   cursor: pointer;
+  font-size: 16px;
+
   &:hover {
-    /* background-color: #0056b3; */
+    background-color: rgb(220, 160, 50);
   }
 `;
 
-export const ClearButton = styled.button`
-  position: absolute;
-  top: 15px;
-  right: 36px;
-  stroke: ${color.blackPrimary};
-    /* background-color: #dc3545; */
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-  &:hover {
-    /* background-color: #c82333; */
-  }
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 240px;
+  margin-top: 10px;
 `;
