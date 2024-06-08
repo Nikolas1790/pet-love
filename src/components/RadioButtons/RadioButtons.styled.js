c6fe27a41de399c945b5070a5f4801f8464caf82
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-06-08 14:44:39 +0300

    add-redax-price-popularity

--- src/components/RadioButtons/RadioButtons.styled.js
+++ src/components/RadioButtons/RadioButtons.styled.js
@@ -1,16 +1,13 @@
 import color from 'common/GlobalColors';
 import styled from 'styled-components';
-// import color from 'common/GlobalColors';
 
 export const RadioContainer = styled.div`
   display: flex;
-  /* flex-direction: column; */
   gap: 10px;
 `;
 
 export const RadioGroup = styled.div`
   display: flex;
-  /* flex-direction: column; */
   gap: 8px;
 `;
 
@@ -24,7 +21,6 @@ export const Label = styled.label`
   align-items: center;
   justify-content: center;
   padding: 14px;
-  /* border: 1px solid #ccc; */
   border-radius: 30px;
   cursor: pointer;
   background-color: ${({ checked }) => (checked ? color.orangePrimary : color.white)};
@@ -39,16 +35,11 @@ export const Label = styled.label`
 
 export const StyledRadioButton = styled.input`
   display: none;
-
-  /* &:checked + ${Label} {
-    background-color: #007bff;
-    color: #fff;
-  } */
 `;
 
 export const SvgX = styled.svg`
-width: 18px;
-height: 18px;
+  width: 18px;
+  height: 18px;
   stroke: ${color.white};
   margin-left: 8px;
 `;
