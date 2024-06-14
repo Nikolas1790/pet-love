ae88dbde31fe207a3a79940f8d242f9d93e682c1
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-06-14 14:26:27 +0300

    user-card-block-style

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -2,15 +2,16 @@ import EditUserBtn from "components/Buttons/EditUserBtn/EditUserBtn";
 import LogOutBtn from "components/Buttons/LogOutBtn/LogOutBtn";
 import PetsBlock from "components/PetsBlock/PetsBlock";
 import UserBlock from "components/UserBlock/UserBlock";
+import { UserCardBlock } from "./UserCard.styled";
 
 export default function UserCard() {
 
   return (
-    <div>
+    <UserCardBlock>
       <EditUserBtn />
       <UserBlock />
       <PetsBlock />
       <LogOutBtn />
-    </div>    
+    </UserCardBlock>    
   );
 }  
---
+++ src/components/UserCard/UserCard.styled.js
@@ -0,0 +1,12 @@
+import color from "common/GlobalColors";
+import styled from "styled-components";
+
+export const UserCardBlock = styled.div`
+  width: 520px;
+  height: 928px;
+  background: ${color.white};
+  border-radius: 60px;
+  margin-right: 32px;
+  padding: 40px;
+`;
+
