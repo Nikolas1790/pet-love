7df241bbfe3c8d7245c818fd97c150753fde2b08
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-06-14 18:16:58 +0300

    photo-container

--- src/components/UserBlock/UserBlock.jsx
+++ src/components/UserBlock/UserBlock.jsx
@@ -1,9 +1,15 @@
+import sprite from '../../img/sprite.svg';
+import { PhotoContainer, UploadPhoto, UserBlockContainer } from './UserBlock.styled';
 
 export default function UserBlock() {
-
   return (
-    <div>
-      eeeeeeeeeeeee
-    </div>    
+    <UserBlockContainer>
+      <PhotoContainer>
+        <svg width={110} height={110}>
+          <use href={`${sprite}#icon-user-without-photo`} />
+        </svg>
+        <UploadPhoto>Upload photo</UploadPhoto>
+      </PhotoContainer>
+    </UserBlockContainer>    
   );
 }  
---
+++ src/components/UserBlock/UserBlock.styled.js
@@ -0,0 +1,22 @@
+
+import styled from "styled-components";
+
+export const UserBlockContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const PhotoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const UploadPhoto = styled.p`
+  font-size: 14px;
+  margin-top: 8px;
+  margin-bottom: 20px;
+`;
