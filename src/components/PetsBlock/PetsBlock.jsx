abfa1b50a0073069b1a37502e45113de523b603f
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-06-19 16:29:11 +0300

    pets-list-layout

--- src/components/PetsBlock/PetsBlock.jsx
+++ src/components/PetsBlock/PetsBlock.jsx
@@ -1,10 +1,12 @@
 import AddPetComponent from "components/AddPetComponent/AddPetComponent";
+import PetsList from "components/PetsList/PetsList";
 
 export default function PetsBlock() {
 
   return (
     <div>
       <AddPetComponent />
+      <PetsList />
     </div>    
   );
 }  
---
+++ src/components/PetsList/PetsList.jsx
@@ -0,0 +1,7 @@
+export default function PetsList() {
+  return (
+    <div>
+      nnnnnnnnnnnnnnnnn
+    </div>
+  );
+}
