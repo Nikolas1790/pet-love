9d7119be2b243b9a474456b9db3a10446ce3c6bb
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-06-28 18:18:46 +0300

    notice-my

--- src/components/MyNotices/MyNotices.jsx
+++ src/components/MyNotices/MyNotices.jsx
@@ -3,10 +3,12 @@ export default function MyNotices() {
   return (
       <div>
         <div>
+        <div>
           dddddddddddddddd
         </div>
         <div>
           rrrrrrrrrrrrrrrr
+        </div>
         </div>
       </div>    
   );
