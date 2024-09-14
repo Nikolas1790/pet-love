ee4357421ec33b4d334486e26d8d0730bd3939ff
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-09-14 14:50:24 +0300

    CORRECT addpet component

--- src/components/AddPet/AddPet.jsx
+++ src/components/AddPet/AddPet.jsx
@@ -3,7 +3,7 @@ import { ContentContainer } from "common/GiobalStyles";
 export default function AddPet() {
   return (
     <ContentContainer>
-      AddPettt
+      AddPet
     </ContentContainer>
   );
 }
