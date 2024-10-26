aa607e1294f5fc12e778bc234de83d385f3329ce
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-10-26 23:47:42 +0300

    main

--- src/components/Buttons/ButtonLightOrange/ButtonLightOrange.jsx
+++ src/components/Buttons/ButtonLightOrange/ButtonLightOrange.jsx
@@ -1,6 +1,5 @@
 import { LightBtn } from "./ButtonLightOrange.styled";
 
-
 export default function ButtonLightOrange({ className, label, onClick, width }) {
   return (    
     <LightBtn className={className} onClick={onClick} width={width} label={label} >
--- src/components/Buttons/ButtonNav/ButtonNav.jsx
+++ src/components/Buttons/ButtonNav/ButtonNav.jsx
@@ -6,8 +6,6 @@ export default function ButtonNav({ className, label, onClick, width, to, home }
     <NavBtn to={to} className={className} onClick={onClick} width={width} label={label} home={home} >
       {label}
 
-
-
       {label === 'Add pet' && (
         <svg width={18} height={18}>
           <use href={`${sprite}#icon-plus`} />
