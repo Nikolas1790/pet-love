ad8568eaf8b2388fa7dae1143130d0186d4acc87
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2025-01-03 23:52:25 +0200

    NewsItem-function-formatting-date

--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -5,7 +5,7 @@ export default function NewsItem({article}) {
     function formatDate(dateString) {
       const date = new Date(dateString);
       const day = date.getDate().toString().padStart(2, '0');
-      const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Месяцы начинаются с 0
+      const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
       const year = date.getFullYear();
       return `${day}/${month}/${year}`;
     }
