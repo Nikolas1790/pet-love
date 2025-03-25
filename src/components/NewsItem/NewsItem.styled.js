39cfb5a5519396af92cb00b450f473324183b5ad
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2025-03-25 23:54:48 +0200

    NewsItem-styles

--- src/components/NewsItem/NewsItem.styled.js
+++ src/components/NewsItem/NewsItem.styled.js
@@ -40,7 +40,7 @@ export const TextArticle = styled.p`
   -webkit-line-clamp: 4; /* Ограничивает количество строк до 4 */
   -webkit-box-orient: vertical; 
   overflow: hidden;
-  text-overflow: ellipsis; /* Добавляет многоточие, если текст обрезается */
+  text-overflow: ellipsis;
   height: 5em; /* Высота, рассчитанная как line-height * количество строк */
 `
 
